c995608f7cca33a5dd0f000a704c15f13f81cef5
karoliny bispo <karol-bobio@hotmail.com>
2024-12-02 09:55:41 -0300

    tirei uma importacao que nao estava sendo usanda

--- 1_componestes/src/componentes/PropsExample.jsx
+++ 1_componestes/src/componentes/PropsExample.jsx
@@ -1,8 +1,5 @@
 /* eslint-disable react/prop-types */
 
-
-import React from "react";
-
 //acesso o onb props, entro na propriedade nome e o dado dinamico aparece ao user
 const PropsExample = (props) => {
     return(
