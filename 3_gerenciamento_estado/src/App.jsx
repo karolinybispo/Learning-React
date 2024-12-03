86995de25c34b096bc1a8120b753a8a2e161f43b
karoliny bispo <karol-bobio@hotmail.com>
2024-12-02 22:31:52 -0300

    exemplo sem state

--- 3_gerenciamento_estado/src/App.jsx
+++ 3_gerenciamento_estado/src/App.jsx
@@ -1,12 +1,12 @@
 
-import './App.css'
-import { UseStateComponent } from './componentes/UseStateComponent'
+import './App.css' //necessario para componente filho ser usado
+import { UseStateComponent } from './componentes/UseStateComponent' // necessario para componente filho ser usado
 
 function App() {
 
   return (
     <>
-      <UseStateComponent/>
+      <UseStateComponent/> {/*componente filho mencionado para que tudo que tiver nele apareca ao user*/}
     </>
   )
 }
--- 3_gerenciamento_estado/src/componentes/UseStateComponent.jsx
+++ 3_gerenciamento_estado/src/componentes/UseStateComponent.jsx
@@ -1,6 +1,43 @@
 
-export const UseStateComponent = () => {
-  return (
-    <div>UseStateComponent</div>
-  )
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+    export const UseStateComponent = () => {
+    //exemplo sem uso do state
+      let count = 0;
+
+      const increment = () => {
+
+        count += 1;
+        console.log(count);
+      };
+
+
+
+      return (
+      <div>
+        <h2>
+          Contador
+        </h2>
+        <p>voce clicou {count} vezes </p>
+        <button onClick={increment}> incrementar </button>
+      </div>  
+      )
+    }
+*/
