f867ced8c56a27c256d0bc16d16eb4f97e1165a5
karoliny bispo <karol-bobio@hotmail.com>
2024-12-09 14:26:31 -0300

    comentarios no arquivo main.jsx e index.html

--- ProgramacaoWbe/P-projeto1/src/main.jsx
+++ ProgramacaoWbe/P-projeto1/src/main.jsx
@@ -1,10 +1,14 @@
+//importacoes
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client' //trabalha com react para poder manipular o DOM
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// aqui esta renderizando os compoenntes no index.html
+
+//METODO PARA REDERIZAR
+createRoot(document.getElementById('root')).render( // o createRoot eh um metodo que cria uma rota onde vai ser renderizado os compoentes, esta pegando um docuemnto pelo id que eh nomeado como root (essa root eh a div dentro do Index.html) que sera render (renderizado) um componente App
+  <StrictMode> 
     <App />
   </StrictMode>,
 )
