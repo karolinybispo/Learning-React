68872eded9c6feef7d63d2ab8e1e25b29778491c
karoliny bispo <karol-bobio@hotmail.com>
2025-01-30 14:39:07 -0300

    criando o projeto de tarefas

--- FelipeRochaAulaYoutube/src/App.jsx
+++ FelipeRochaAulaYoutube/src/App.jsx
@@ -1,11 +1,19 @@
+import AddTasks from "./componentes/AddTasks";
+import Tasks from "./componentes/Tasks";
 
 //criando primeiro componente
 function App(){
-  const message = 'oiie';
+  //const message = 'oiie';
   return(
     <div>
-        <h2>{message}</h2>
-        <h3>como vai?</h3>
+    {/* <h2>{message}</h2>*/} 
+    {/*  <h3>como vai?</h3>*/}
+
+    <h1>Gerenciador de tarefas</h1>
+    <AddTasks></AddTasks>
+    <Tasks></Tasks>
+
+
     </div>
   );
 }
--- FelipeRochaAulaYoutube/src/main.jsx
+++ FelipeRochaAulaYoutube/src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import EstadoState from './estadoState.jsx'
+//import EstadoState from './estadoState.jsx'
 
 createRoot(document.getElementById('root')).render( //aqui esta sendo flado para o react criar o react, renderizar dentro da aplicacao com id root, que fica dentro do index.html
   <StrictMode>
     <App />
-    <EstadoState></EstadoState>
-  </StrictMode>,
+  {/*  <EstadoState></EstadoState> */}
+    </StrictMode>,
 )
